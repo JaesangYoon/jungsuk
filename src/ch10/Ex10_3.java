8d004aebd8ca195f907df2265ad0742f0da247c6
jaesangyoon <jsangyoun@gmail.com>
2022-08-21 23:31:21 +0900

    test

--- src/ch10/Ex10_3.java
+++ src/ch10/Ex10_3.java
@@ -4,6 +4,20 @@ import java.util.Calendar;
 
 public class Ex10_3 {
     public static void main(String[] args) {
+        final int[] TIME_UNIT = {3600, 60, 1};
+        final String[] TIME_UNIT_NAME = {"시간 ", "분 ", "초 "};
+
+        Calendar time1 = Calendar.getInstance();
+        Calendar time2 = Calendar.getInstance();
+
+        time1.set(Calendar.HOUR_OF_DAY, 10);
+        time1.set(Calendar.MINUTE, 20);
+        time1.set(Calendar.SECOND, 30); // time1: 10시 20분 30초
+
+
+        // time2: 20시 30분 10초
+
+
 
 
     }
