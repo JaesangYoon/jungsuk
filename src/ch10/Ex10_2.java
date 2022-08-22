1a3d888891ed5d16e85536dedcc69d94c5e850f0
jaesangyoon <jsangyoun@gmail.com>
2022-08-22 16:19:25 +0900

    test

--- src/ch10/Ex10_2.java
+++ src/ch10/Ex10_2.java
@@ -17,10 +17,10 @@ public class Ex10_2 {
         Calendar date2 = Calendar.getInstance();
 
         System.out.println(date1); // 현재 날짜, 시간
-//        date1.set(2019, 3, 29);
-//        System.out.println(toString(date1) + " " + DAY_OF_WEEK[date1.get(Calendar.DAY_OF_WEEK)] + "요일입니다.");
-//        System.out.println(toString(date2) + " " + DAY_OF_WEEK[date2.get(Calendar.DAY_OF_WEEK)] + "요일입니다.");
-//        System.out.println(date2);
+        date1.set(2019, 3, 29);
+        System.out.println(toString(date1) + " " + DAY_OF_WEEK[date1.get(Calendar.DAY_OF_WEEK)] + "요일입니다.");
+        System.out.println(toString(date2) + " " + DAY_OF_WEEK[date2.get(Calendar.DAY_OF_WEEK)] + "요일입니다.");
+        System.out.println(date2);
 
         // 두 날짜간의 차이를 얻으려면, getTimeInMillis() 천분의 일초 단위로 변환해야 한다.
         long difference = (date2.getTimeInMillis() - date1.getTimeInMillis()) / 1000;
--- src/ch10/Ex10_3.java
+++ src/ch10/Ex10_3.java
@@ -3,6 +3,7 @@ package ch10;
 import java.util.Calendar;
 
 public class Ex10_3 {
+
     public static void main(String[] args) {
         final int[] TIME_UNIT = {3600, 60, 1};
         final String[] TIME_UNIT_NAME = {"시간 ", "분 ", "초 "};
@@ -15,10 +16,31 @@ public class Ex10_3 {
         time1.set(Calendar.SECOND, 30); // time1: 10시 20분 30초
 
 
-        // time2: 20시 30분 10초
+        time2.set(Calendar.HOUR_OF_DAY, 20);
+        time2.set(Calendar.MINUTE, 30);
+        time2.set(Calendar.SECOND, 10); // time2: 20시 30분 10초
+
+        System.out.println(time2.HOUR_OF_DAY);
+        System.out.println(time2.get(Calendar.HOUR_OF_DAY));
+
+        // 시간 출력하기
+        System.out.println("time1: " + time1.get(Calendar.HOUR_OF_DAY) + "시 " + time1.get(Calendar.MINUTE) + "분 " + time1.get(Calendar.SECOND) + "초");
+        System.out.println("time2: " + time2.get(Calendar.HOUR_OF_DAY) + "시 " + time2.get(Calendar.MINUTE) + "분 " + time2.get(Calendar.SECOND) + "초");
 
 
+        // time1과 time2의 차이 구하기
 
+        long difference = Math.abs(time2.getTimeInMillis() - time1.getTimeInMillis()) / 1000;
+        System.out.println("time1과 time2의 차이는 " + difference + "입니다.");
 
+        String tmp = "";
+        for (int i = 0; i < TIME_UNIT.length; i++) {
+            tmp += difference / TIME_UNIT[i] + TIME_UNIT_NAME[i];
+            difference %= TIME_UNIT[i];
+        }
+        System.out.println("시분초로 변환하면 " + tmp + "입니다.");
+    }
+    public static String toString(Calendar date) {
+        return date.get(Calendar.YEAR) + "년 " + date.get(Calendar.MONTH) + "월 " + date.get(Calendar.DATE) + "일";
     }
 }
