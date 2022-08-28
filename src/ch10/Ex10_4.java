9c71700f711bbced067eb5adaac701bc04b673a0
jaesangyoon <jsangyoun@gmail.com>
2022-08-28 18:31:52 +0900

    test

--- src/ch10/Ex10_4.java
+++ src/ch10/Ex10_4.java
@@ -1,8 +1,33 @@
 package ch10;
 
-public class Ex10_4 {
-    public static void main(String[] args) {
+import java.util.Calendar;
+import java.util.Date;
 
+public class Ex10_4 {
+    public static String toString(Calendar date) {
+        return date.get(Calendar.YEAR) + "년 " + date.get(Calendar.MONTH) + "월 " + date.get(Calendar.DATE) + "일";
+    }
+
+    public static void main(String[] args) {
+        Calendar date = Calendar.getInstance();
+        date.set(2019, 7, 31);
+
+        System.out.println(toString(date));
+        System.out.println("= 1일 후 =");
+        date.add(Calendar.DATE, 1);
+        System.out.println(toString(date));
+
+        System.out.println("= 6달 전 =");
+        date.add(Calendar.MONTH, -6);
+        System.out.println(toString(date));
+
+        System.out.println("= 31일 후(roll) =");
+        date.roll(Calendar.DATE, 31);
+        System.out.println(toString(date));
+
+        System.out.println("= 31일 후(add) =");
+        date.add(Calendar.DATE, 31);
+        System.out.println(toString(date));
 
     }
 }
